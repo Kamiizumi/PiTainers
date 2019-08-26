06b96ea5ea542320e76a659894f12f6e00b3bc56
Ben Newman <BenNewman@outlook.com>
2019-08-27 00:32:15 +0100

    Add ds18b20-mqtt container (#1)

---
+++ ds18b20-mqtt/script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+while true;
+do
+  TEMPERATURE="$(cat /sys/bus/w1/devices/$1/w1_slave | awk 'END{print}' | awk 'NF>1{print $NF}' | awk '{print substr($0,3)/1000}')"; #| mosquitto_pub -h pi3-bplus.lan --id $1 -t ds18b20/$1 -l;
+  echo "$(date): ${TEMPERATURE}";
+  echo "Sending to mqtt...";
+  mosquitto_pub -h pi3-bplus.lan --id $1 -t ds18b20/$1 -m "${TEMPERATURE}";
+  echo "Finished";
+  echo "";
+  sleep 1;
+done;
