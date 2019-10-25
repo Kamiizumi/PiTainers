59650d55bb008a29acf634e84a7d6738d72e36e9
Ben Newman <ben@newman.ws>
2019-10-25 23:11:09 +0100

    Delete commented code

--- ds18b20-mqtt/script.sh
+++ ds18b20-mqtt/script.sh
@@ -2,7 +2,7 @@
 
 while true;
 do
-  TEMPERATURE="$(cat /sys/bus/w1/devices/$1/w1_slave | awk 'END{print}' | awk 'NF>1{print $NF}' | awk '{print substr($0,3)/1000}')"; #| mosquitto_pub -h pi3-bplus.lan --id $1 -t ds18b20/$1 -l;
+  TEMPERATURE="$(cat /sys/bus/w1/devices/$1/w1_slave | awk 'END{print}' | awk 'NF>1{print $NF}' | awk '{print substr($0,3)/1000}')";
   echo "$(date): ${TEMPERATURE}";
   echo "Sending to mqtt...";
   mosquitto_pub -h pi3-bplus.lan --id $1 -t ds18b20/$1 -m "${TEMPERATURE}";
