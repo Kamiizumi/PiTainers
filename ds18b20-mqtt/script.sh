fed20d29734c77999100772e6632bebb3c1f996e
Ben Newman <ben@newman.ws>
2019-10-25 23:18:49 +0100

    Get MQTT host from params passed to script

--- ds18b20-mqtt/script.sh
+++ ds18b20-mqtt/script.sh
@@ -4,8 +4,8 @@ while true;
 do
   TEMPERATURE="$(cat /sys/bus/w1/devices/$1/w1_slave | awk 'END{print}' | awk 'NF>1{print $NF}' | awk '{print substr($0,3)/1000}')";
   echo "$(date): ${TEMPERATURE}";
-  echo "Sending to mqtt...";
-  mosquitto_pub -h pi3-bplus.lan --id $1 -t ds18b20/$1 -m "${TEMPERATURE}";
+  echo "Sending to mqtt host: $2...";
+  mosquitto_pub -h $2 --id $1 -t ds18b20/$1 -m "${TEMPERATURE}";
   echo "Finished";
   echo "";
   sleep 1;
